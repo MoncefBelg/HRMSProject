00be47992e035c61d1bd4eb7d542cfc33e4eea9d
daboss0580 <iamjustin0580@gmail.com>
2023-07-13 10:49:45 +0100

    Utils package is ready

---
+++ src/test/java/UtilsByJustin/Constants.java
@@ -0,0 +1,7 @@
+package UtilsByJustin;
+
+public class Constants {
+    public static final String CONFIG_READER_PATH = System.getProperty("user.dir") + "/src/test/resources/Config/Config.properties";
+    public static final String SCREENSHOT_FILE_PATH = System.getProperty("user.dir") + "/screenshots/";
+    public static final String EXCEL_READER_PATH = System.getProperty("user.dir") + "/src/test/resources/TestData/Group18Project.xlsx";
+}

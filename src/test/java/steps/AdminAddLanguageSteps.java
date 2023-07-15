a68ca6e1902c7e1cedae009da1f2e22fcfa8e6c9
AWahabak <128252012+AWahabak@users.noreply.github.com>
2023-07-15 05:29:48 +0100

    Intial UserStory15 Admin to add language test case with log

--- src/test/java/steps/AdminAddLanguageSteps.java
+++ src/test/java/steps/AdminAddLanguageSteps.java
@@ -2,25 +2,34 @@ package steps;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.log4j.xml.DOMConfigurator;
 import pages.AQualificationLanguagePage;
 import utils.CommonMethods;
+import utils.ConfigReader;
+import utils.Log;
 
 public class AdminAddLanguageSteps extends CommonMethods {
 
     @When("user navigates to Qualification and selects Languages")
     public void user_navigates_to_qualification_and_selects_languages() {
-        //System.out.println("Admin navigates to qualification");
+        DOMConfigurator.configure("log4j.xml");
+        Log.startTestCase("Start test case for Admin Add Language");
         //System.out.println("Admin select language from qualification list");
         click(aQualificationLanguagePage.adminModuleTab);
+        Log.info("Admin navigates to Admin module");
         click(aQualificationLanguagePage.qualificationDropdown);
+        Log.info("Admin navigates to Qualifications menu");
         click(aQualificationLanguagePage.languagesOptionList);
+        Log.info("Admin navigates to Language page");
     }
     @Then("user can add any language")
     public void user_can_add_any_language() {
-        //System.out.println("Admin adds the intended language");
         click(aQualificationLanguagePage.addLanguageButton);
-        sendText("Acehnese",aQualificationLanguagePage.languageField);
+        Log.info("Admin clicks on add language button");
+        sendText(ConfigReader.getPropertyValue("language"),aQualificationLanguagePage.languageField);
+        Log.info("Admin enters the intended language in language name field");
         click(aQualificationLanguagePage.saveLanguageButton);
-        //System.out.println("Admin verifies if the intended language is added");
+        Log.info("Admin clicks on save button");
+        Log.endTestCase("The end of test case for Admin Add Language");
     }
 }

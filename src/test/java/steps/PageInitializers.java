11e16970dba0b5fe1f9be56b7ae46eca84e55e53
MoncefBelg <moncefbe@hotmail.com>
2023-07-16 21:06:04 +0100

    The user story 5 is completed

--- src/test/java/steps/PageInitializers.java
+++ src/test/java/steps/PageInitializers.java
@@ -1,14 +1,19 @@
 package steps;
 
 import pages.DashboardPage;
+import pages.EmployeePage;
 import pages.LoginPage;
 
 public class PageInitializers {
     public static LoginPage loginPage;
     public static DashboardPage dashboardPage;
 
+    public static EmployeePage employeePage;
+
     public static void initializePageObject(){
         loginPage = new LoginPage();
         dashboardPage = new DashboardPage();
+        employeePage = new EmployeePage();
+
     }
 }

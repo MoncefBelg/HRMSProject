f503e3081f8898c14c7ef6ae3da2d324a9584c67
daboss0580 <iamjustin0580@gmail.com>
2023-07-17 20:16:52 +0100

    Justin's user story

---
+++ src/test/java/pages/TerminateEmployeePage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import steps.TerminateEmployees;
+import utils.CommonMethods;
+
+public class TerminateEmployeePage extends CommonMethods {
+
+    @FindBy(xpath = "//b[text()='PIM']")
+    public WebElement pimOption;
+    @FindBy(xpath = "//a[text()='Employee List']")
+    public WebElement empList;
+    @FindBy(xpath = "//input[@name='empsearch[id]']")
+    public WebElement empSearchField;
+    @FindBy(xpath = "//input[@id='searchBtn']")
+    public WebElement empSearchBtn;
+    @FindBy(xpath = "//table[@class='table hover']//tbody[1]//td[2]//a")
+    public WebElement empSearchResult;
+    @FindBy(xpath = "//ul[@id='sidenav']//li[6]//a")
+    public WebElement jobBtn;
+    @FindBy(xpath = "//input[@id='btnTerminateEmployement']")
+    public WebElement terminateBtn;
+    @FindBy(xpath = "//input[@id='dialogConfirm']")
+    public WebElement terminationConfirm;
+    @FindBy(xpath = "//label[@id='terminatedDate']")
+    public WebElement confirmationMessage;
+
+    public TerminateEmployeePage(){
+        PageFactory.initElements(driver,this);
+    }
+}
--- src/test/java/steps/PageInitializers.java
+++ src/test/java/steps/PageInitializers.java
@@ -2,13 +2,16 @@ package steps;
 
 import pages.DashboardPage;
 import pages.LoginPage;
+import pages.TerminateEmployeePage;
 
 public class PageInitializers {
     public static LoginPage loginPage;
     public static DashboardPage dashboardPage;
+    public static TerminateEmployeePage terminateEmployees;
 
     public static void initializePageObject(){
         loginPage = new LoginPage();
         dashboardPage = new DashboardPage();
+        terminateEmployees=new TerminateEmployeePage();
     }
 }
